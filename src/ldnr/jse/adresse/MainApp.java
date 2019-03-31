15da640721af09a2eb8090699db5e5acc7a99c9d
AceCerf <tiangao1993@gmail.com>
2019-03-31 03:03:18 +0200

    ajouté observalist pour les trois modèles

--- src/ldnr/jse/adresse/MainApp.java
+++ src/ldnr/jse/adresse/MainApp.java
@@ -4,48 +4,117 @@
  * and open the template in the editor.
  */
 package ldnr.jse.adresse;
+  
+import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import ldnr.jse.adresse.model.Commercial;
+import ldnr.jse.adresse.model.PersonneMorale;
+import ldnr.jse.adresse.model.PersonnePhysique;
+import ldnr.jse.adresse.view.ApercuPersController;
 
-/**
- *
- * @author stag
- */
 public class MainApp extends Application {
-    
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    /**
+     * The data as an observable list of PersonnePhysique, PersonneMorale, Commecial.
+     */
+    private final ObservableList<PersonnePhysique> personnePhysiqueData = FXCollections.observableArrayList();
+    private final ObservableList<PersonneMorale> personneMoraleData = FXCollections.observableArrayList();
+    private final ObservableList<Commercial> commercialData = FXCollections.observableArrayList();
+
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personnePhysiqueData.add(new PersonnePhysique("Alan", "Walker"));
+        personnePhysiqueData.add(new PersonnePhysique("Ed", "Sheeran"));
+        personneMoraleData.add(new PersonneMorale("SONY Music"));
+        personneMoraleData.add(new PersonneMorale("Universal Music"));
+        commercialData.add(new Commercial("Victoria", "Buckerham"));
+        commercialData.add(new Commercial("David", "Buckerham"));
+    }
+  
+    /**
+     * Returns the data as an observable list of all three models. 
+     * @return
+     */
+    public ObservableList<PersonnePhysique> getPersonnePhysiqueData() {
+        return personnePhysiqueData;
+    }
+    public ObservableList<PersonneMorale> getPersonneMoraleData() {
+        return personneMoraleData;
+    }
+    public ObservableList<Commercial> getCommercialData() {
+        return commercialData;
+    }
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+    
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
             
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+        }
     }
 
     /**
-     * @param args the command line arguments
+     * Shows the person overview inside the root layout.
      */
+    public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+            
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+            
+            // Give the controller access to the main app.
+            ApercuPersController controller = loader.getController();
+            controller.setMainApp(this);
+        } catch (IOException e) {
+        }
+    }
+    
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
